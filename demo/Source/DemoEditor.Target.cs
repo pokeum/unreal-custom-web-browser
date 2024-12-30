8569d56d1c98ec8c9d5d92fc154827fdd64ad2e7
pokeum <pokeum@ab180.co>
2024-12-31 00:30:45 +0900

    Unreal Engine v5.4

--- demo/Source/DemoEditor.Target.cs
+++ demo/Source/DemoEditor.Target.cs
@@ -8,8 +8,9 @@ public class DemoEditorTarget : TargetRules
 	public DemoEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "Demo" } );
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("Demo");
 		
 		if (Target.Platform == UnrealTargetPlatform.Mac)
 		{
